2b8b5c5fcd11610be4e12bddd9dcc8bb7d947f9a
ognjenPop <ognjenpopovic1326@gmail.com>
2026-04-11 20:56:32 +0200

    Login i register forma

--- app/src/main/java/ognjen/popovic/eventsapp/MainActivity.java
+++ app/src/main/java/ognjen/popovic/eventsapp/MainActivity.java
@@ -2,11 +2,14 @@ package ognjen.popovic.eventsapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     LinearLayout startLayout;
     LinearLayout loginLayout;
@@ -16,6 +19,9 @@ public class MainActivity extends Activity {
     Button btnRegister;
     Button btnDoLogin;
     Button btnDoRegister;
+
+    EditText etLoginUsername;
+    EditText etLoginPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,10 @@ public class MainActivity extends Activity {
         btnDoLogin = findViewById(R.id.btnDoLogin);
         btnDoRegister = findViewById(R.id.btnDoRegister);
 
+        etLoginUsername = findViewById(R.id.etLoginUsername);
+        etLoginPassword = findViewById(R.id.etLoginPassword);
+
+        // Klik na LOGIN dugme (prikaz forme)
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,12 +50,29 @@ public class MainActivity extends Activity {
             }
         });
 
+        // Klik na REGISTER dugme (prikaz forme)
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startLayout.setVisibility(View.GONE);
                 registerLayout.setVisibility(View.VISIBLE);
                 loginLayout.setVisibility(View.GONE);
+            }
+        });
+
+        // LOGIN logika
+        btnDoLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = etLoginUsername.getText().toString();
+                String password = etLoginPassword.getText().toString();
+
+                if(username.equals("admin") && password.equals("admin")) {
+                    Toast.makeText(MainActivity.this, "Login uspešan", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Pogrešan username ili password", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
