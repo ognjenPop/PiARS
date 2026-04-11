9c1c791b27d3731b70344b01ceb95eb9ea8b7019
ognjenPop <ognjenpopovic1326@gmail.com>
2026-04-11 19:44:35 +0200

    drugi commit

--- app/src/main/java/ognjen/popovic/eventsapp/MainActivity.java
+++ app/src/main/java/ognjen/popovic/eventsapp/MainActivity.java
@@ -1,13 +1,52 @@
 package ognjen.popovic.eventsapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    LinearLayout startLayout;
+    LinearLayout loginLayout;
+    LinearLayout registerLayout;
+
+    Button btnLogin;
+    Button btnRegister;
+    Button btnDoLogin;
+    Button btnDoRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startLayout = findViewById(R.id.startLayout);
+        loginLayout = findViewById(R.id.loginLayout);
+        registerLayout = findViewById(R.id.registerLayout);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnDoLogin = findViewById(R.id.btnDoLogin);
+        btnDoRegister = findViewById(R.id.btnDoRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLayout.setVisibility(View.GONE);
+                loginLayout.setVisibility(View.VISIBLE);
+                registerLayout.setVisibility(View.GONE);
+            }
+        });
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startLayout.setVisibility(View.GONE);
+                registerLayout.setVisibility(View.VISIBLE);
+                loginLayout.setVisibility(View.GONE);
+            }
+        });
     }
 }
