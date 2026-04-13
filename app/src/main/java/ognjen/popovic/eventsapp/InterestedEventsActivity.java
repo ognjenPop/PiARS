d6d203e6881af86b043d2ae9a9b7bb04c77eeb4b
ognjenPop <ognjenpopovic1326@gmail.com>
2026-04-13 22:58:59 +0200

    MyEventFragment.java

---
+++ app/src/main/java/ognjen/popovic/eventsapp/InterestedEventsActivity.java
@@ -0,0 +1,13 @@
+package ognjen.popovic.eventsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InterestedEventsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interested_events);
+    }
+}
--- app/src/main/java/ognjen/popovic/eventsapp/MyEventsFragment.java
+++ app/src/main/java/ognjen/popovic/eventsapp/MyEventsFragment.java
@@ -1,20 +1,71 @@
 package ognjen.popovic.eventsapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MyEventsFragment extends Fragment {
 
+    Button btnInterestedEvents;
+    Button btnAttendingEvents;
+    Button btnMyProfile;
+
     public MyEventsFragment() {
-        // prazan konstruktor
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_my_events, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_my_events, container, false);
+
+        btnInterestedEvents = view.findViewById(R.id.btnInterestedEvents);
+        btnAttendingEvents = view.findViewById(R.id.btnAttendingEvents);
+        btnMyProfile = view.findViewById(R.id.btnMyProfile);
+
+        btnInterestedEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), InterestedEventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnAttendingEvents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getActivity(), AttendingEventsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btnMyProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Bundle bundleFromActivity = getActivity().getIntent().getExtras();
+
+                String username = "";
+                String email = "";
+
+                if (bundleFromActivity != null) {
+                    username = bundleFromActivity.getString("username");
+                    email = bundleFromActivity.getString("email");
+                }
+
+                Intent intent = new Intent(getActivity(), ProfileActivity.class);
+                intent.putExtra("username", username);
+                intent.putExtra("email", email);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
