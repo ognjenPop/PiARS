9645f931790acbe1e7b4710980e80ecef6130264
ognjenPop <ognjenpopovic1326@gmail.com>
2026-04-13 23:29:43 +0200

    Interested i attending events trenutno blanco

--- app/src/main/java/ognjen/popovic/eventsapp/AttendingEventsActivity.java
+++ app/src/main/java/ognjen/popovic/eventsapp/AttendingEventsActivity.java
@@ -1,10 +1,9 @@
 package ognjen.popovic.eventsapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class AttendingEventsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
